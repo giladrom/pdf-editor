0656373ce33ec145ec018492a3c570608421db92
giladrom <gilad.rom@gmail.com>
2024-08-20 17:50:16 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,29 @@
 import { Text, Title, Center, Code } from "@mantine/core";
+import { Uploader } from "./_components/uploader";
 
 export default async function Home() {
   return (
     <>
-      <Center>
-        <Title>Mantine, TRPC, Prisma, NextAuth + App Directory Template</Title>
-      </Center>
+      <div className="p-15 m-15 flex h-screen flex-col items-center justify-center">
+        <Center>
+          <Title>
+            Mantine, TRPC, Prisma, NextAuth + App Directory Template
+          </Title>
+        </Center>
 
-      <Center>
-        <Text>🎈 Mantine + T3 Stack Starter Kit</Text>
-      </Center>
+        <Center>
+          <Text>🎈 Mantine + T3 Stack Starter Kit</Text>
+        </Center>
 
-      <Center>
-        <Text>Edit <Code>src/app/page.tsx</Code> to get started</Text>
-      </Center>
+        <Center>
+          <Text>
+            Edit <Code>src/app/page.tsx</Code> to get started
+          </Text>
+        </Center>
+        <div className="w-1/2">
+          <Uploader />
+        </div>
+      </div>
     </>
-  )
+  );
 }
